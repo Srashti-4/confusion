5d9b660fa262eac215899e15320e3d34270ba3a1
Srashti Gangil <4srashti@gmail.com>
2021-05-18 20:02:23 +0530

    Redux Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem, Button, Col, Label, Row } from 'reactstrap';
 // import { Form, FormFeedback, FormGroup, Input} from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Control, Form, Errors } from 'react-redux-form';
+// import { actions } from 'react-redux-form';
 
 const required = (val) => val && val.length;
-const maxLength = (len)  => (val) => !(val) || (val.length <= len);
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
@@ -19,7 +19,7 @@ class Contact extends Component {
     constructor(props) {
         super(props);
 
-/* Will be done by react-redux-form on our behalf */
+        /* Will be done by react-redux-form on our behalf */
         // this.state = {
         //     firstname: '',
         //     lastname: '',
@@ -39,15 +39,15 @@ class Contact extends Component {
         // this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         // this.handleBlur = this.handleBlur.bind(this);
-        
+
     }
 
-/* Will be done by react-redux-form on our behalf */
+    /* Will be done by react-redux-form on our behalf */
     // handleInputChange(event) {
     //     const target = event.target;
     //     const value = target.type === 'checkbox' ? target.checked : target.value;
     //     const name = target.name;
-    
+
     //     this.setState({
     //       [name]: value
     //     });
@@ -56,6 +56,7 @@ class Contact extends Component {
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
 
@@ -65,14 +66,14 @@ class Contact extends Component {
     //     event.preventDefault();
     // }
 
-/* Will be done by react-redux-form on our behalf */
+    /* Will be done by react-redux-form on our behalf */
     // handleBlur = (field) => (evt) => {
     //     this.setState({
     //         touched: { ...this.state.touched, [field]: true}
     //     });
     // }
 
-/* Will be done by react-redux-form on our behalf */
+    /* Will be done by react-redux-form on our behalf */
     // validate(firstname, lastname, telnum, email) {
     //     const errors = {
     //         firstname: '',
@@ -105,32 +106,32 @@ class Contact extends Component {
 
         // const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
 
-        return(
+        return (
             <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Contact US</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Contact Us</h3>
-                    <hr />
-                </div>           
-            </div>
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact US</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Contact Us</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row row-content">
                     <div className="col-12">
-                    <h3>Location Information</h3>
+                        <h3>Location Information</h3>
                     </div>
                     <div className="col-12 col-sm-4 offset-sm-1">
-                            <h5>Our Address</h5>
-                            <address>
+                        <h5>Our Address</h5>
+                        <address>
                             121, Clear Water Bay Road<br />
                             Clear Water Bay, Kowloon<br />
                             HONG KONG<br />
                             <i className="fa fa-phone"></i>: +852 1234 5678<br />
                             <i className="fa fa-fax"></i>: +852 8765 4321<br />
                             <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                            </address>
+                        </address>
                     </div>
                     <div className="col-12 col-sm-6 offset-sm-1">
                         <h5>Map of our Location</h5>
@@ -148,25 +149,25 @@ class Contact extends Component {
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name="firstname"
                                         placeholder="First Name"
                                         className="form-control"
-                                        validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                         />
-                                         <Errors
-                                             className="text-danger"
-                                             model=".lastname"
-                                             show="touched"
-                                             messages={{
-                                                 required: 'Required',
-                                                 minLength: 'Must be greater than 2 characters',
-                                                 maxLength: 'Must be 15 characters or less'
-                                             }}
-                                          />
+                                        validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -175,18 +176,18 @@ class Contact extends Component {
                                     <Control.text model=".lastname" id="lastname" name="lastname"
                                         placeholder="Last Name"
                                         className="form-control"
-                                        validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                         />
+                                        validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }}
+                                    />
                                     <Errors
-                                             className="text-danger"
-                                             model=".lastname"
-                                             show="touched"
-                                             messages={{
-                                                 required: 'Required',
-                                                 minLength: 'Must be greater than 2 characters',
-                                                 maxLength: 'Must be 15 characters or less'
-                                             }}
-                                          />
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -196,19 +197,19 @@ class Contact extends Component {
                                         placeholder="Tel. Number"
                                         className="form-control"
                                         validators={{ required, minLength: minLength(3), maxLength: maxLength(15), isNumber }}
-                                         />
+                                    />
                                 </Col>
                                 <Errors
-                                        className="text-danger"
-                                        model=".telnum"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 numbers',
-                                            maxLength: 'Must be 15 numbers or less',
-                                            isNumber: 'Must be a number'
-                                        }}
-                                     />
+                                    className="text-danger"
+                                    model=".telnum"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 numbers',
+                                        maxLength: 'Must be 15 numbers or less',
+                                        isNumber: 'Must be a number'
+                                    }}
+                                />
                             </Row>
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label>
@@ -218,28 +219,28 @@ class Contact extends Component {
                                         className="form-control"
                                         validators={{ required, validEmail }} />
                                 </Col>
-                                    <Errors
-                                        className="text-danger"
-                                        model=".email"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            validEmail: 'Invalid Email Address'
-                                        }}
-                                     />
+                                <Errors
+                                    className="text-danger"
+                                    model=".email"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        validEmail: 'Invalid Email Address'
+                                    }}
+                                />
                             </Row>
                             <Row className="form-group">
-                                <Col md={{size: 6, offset: 2}}>
+                                <Col md={{ size: 6, offset: 2 }}>
                                     <div className="form-check">
                                         <Label check>
                                             <Control.checkbox model=".agree" name="agree"
                                                 className="form-check-input"
-                                                 /> {' '}
-                                                <strong>May we contact you?</strong>
+                                            /> {' '}
+                                            <strong>May we contact you?</strong>
                                         </Label>
                                     </div>
                                 </Col>
-                                <Col md={{size: 3, offset: 1}}>
+                                <Col md={{ size: 3, offset: 1 }}>
                                     <Control.select model=".contactType" name="contactType"
                                         className="form-control">
                                         <option>Tel.</option>
@@ -256,13 +257,13 @@ class Contact extends Component {
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Col md={{size:10, offset: 2}}>
+                                <Col md={{ size: 10, offset: 2 }}>
                                     <Button type="submit" color="primary">
-                                    Send Feedback
+                                        Send Feedback
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                         {/* <Form onSubmit={this.handleSubmit}>
                             <FormGroup row>
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreaters';
+import { actions } from 'react-redux-form';
 // import { DISHES } from '../shared/dishes';
 // import { COMMENTS } from '../shared/comments';
 // import { LEADERS } from '../shared/leaders';
@@ -27,7 +28,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes()) }
+  fetchDishes: () => { dispatch(fetchDishes()) },
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 })
 
 class Main extends Component {
@@ -84,7 +86,7 @@ class Main extends Component {
           <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
           <Redirect to="/home" />
         </Switch>
         {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> */}
